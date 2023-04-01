6c90e66ea2591f4b4ebffb9e01b2d6b1ada566db
Alexandru Ion <ionalexaai@gmail.com>
2023-04-01 12:23:28 +0300

    Create logs folder if it doesn't exist

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class BaseConfig:
     HOSTS_FILE = ""
     FIELDS = {
@@ -6,3 +8,10 @@ class BaseConfig:
         "meta": ""
     }
     OUTPUT_FILE = ""
+    LOG_FOLDER = "logs"
+    LOG_FILE = f"{LOG_FOLDER}\\main.log"
+    LOG_FILE_SIZE = 3
+
+
+if not os.path.exists(BaseConfig.LOG_FOLDER):
+    os.makedirs(BaseConfig.LOG_FOLDER)
