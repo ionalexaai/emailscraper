8a16ecbeb14cf910854a7da3477c3efa72f1f056
Alexandru Ion <ionalexaai@gmail.com>
2023-04-01 12:43:32 +0300

    Add timestamps to output files

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
 import os
+import datetime
 
 class BaseConfig:
-    HOSTS_FILE = ""
+    now = datetime.datetime.now()
+    date_string = now.strftime("%Y-%m-%d")
+    time_string = now.strftime("%H-%M-%S")
+
+    HOSTS_FILE = "domains.txt"
+    THREADS_NUMBER = 5
     FIELDS = {
         "title": "",
         "url": "",
         "meta": ""
     }
-    OUTPUT_FILE = ""
+    OUTPUT_FOLDER = "results"
+    OUTPUT_FILE = f"{OUTPUT_FOLDER}\\Results_{date_string}_{time_string}.csv"
     LOG_FOLDER = "logs"
     LOG_FILE = f"{LOG_FOLDER}\\main.log"
     LOG_FILE_SIZE = 3
@@ -15,3 +22,5 @@ class BaseConfig:
 
 if not os.path.exists(BaseConfig.LOG_FOLDER):
     os.makedirs(BaseConfig.LOG_FOLDER)
+if not os.path.exists(BaseConfig.OUTPUT_FOLDER):
+    os.makedirs(BaseConfig.OUTPUT_FOLDER)    
