cad6c2c3f3021d78821e080d735dfa2d9baf6d45
Alexandru Ion <ionalexaai@gmail.com>
2023-04-01 12:55:00 +0300

    Use config file

--- scraper_class.py
+++ scraper_class.py
@@ -4,6 +4,7 @@ from threading import Thread
 import queue
 import re
 from loggers import logger
+from config import BaseConfig
 
 class DomainExplorer(Thread):
     def __init__(self, workerqueue, resultsqueue):
@@ -12,9 +13,9 @@ class DomainExplorer(Thread):
         self.work = workerqueue
         self.results = resultsqueue
         self.r = requests.Session()
-        self.timeout = 10
-        self.r.headers.update({"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"})
-        self.pattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        self.timeout = BaseConfig.REQUEST_TIMEOUT
+        self.r.headers.update(BaseConfig.HEADERS)
+        self.pattern = BaseConfig.EMAIL_PATTERN
         logger.info("Thread Started")
 
 
